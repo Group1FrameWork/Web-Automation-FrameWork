0f0b94021ced0e4c1f35505081578697a483075b
RoySubhra1988 <roysubhra1988@gmail.com>
2018-09-16 19:58:05 -0400

    Welcome to FrameWork

---
+++ Amazon/src/test/java/Search/searchItems.java
@@ -0,0 +1,14 @@
+package Search;
+
+import base.CommonAPI;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
+public class searchItems extends CommonAPI {
+
+    @Test
+    public void search(){
+        driver.findElement(By.cssSelector("#twotabsearchtextbox")).sendKeys("Note 8", Keys.ENTER);
+    }
+}
---
+++ Generic/src/main/java/base/CommonAPI.java
@@ -0,0 +1,32 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import java.util.concurrent.TimeUnit;
+
+public class CommonAPI {
+
+    public WebDriver driver = null;
+
+    @Parameters({"url"})
+    @BeforeMethod
+    public void setUp(@Optional("https://www.amazon.com/") String url)throws Exception{
+        System.setProperty("webdriver.chrome.driver","/Users/subhra/WebPageAutomation/Generic/Browser-Driver/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(25, TimeUnit.SECONDS);
+        driver.get(url);
+
+    }
+
+    @AfterMethod
+    public void cleanUp(){
+        driver.close();
+    }
+}
