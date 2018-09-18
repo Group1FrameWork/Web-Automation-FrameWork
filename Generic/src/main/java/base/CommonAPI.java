ffcafcc5dd0c2998b1b2c6fd3399f3e70a97a440
RoySubhra1988 <roysubhra1988@gmail.com>
2018-09-17 21:50:20 -0400

    Working on commonApi

--- Generic/src/main/java/base/CommonAPI.java
+++ Generic/src/main/java/base/CommonAPI.java
@@ -2,31 +2,107 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class CommonAPI {
 
     public WebDriver driver = null;
+    public String browserstack_username= "your user name";
+    public String browserstack_accesskey = "your access key";
+    public String saucelabs_username = "";
+    public String saucelabs_accesskey = "";
 
-    @Parameters({"url"})
+
+    @Parameters({"useCloudEnv","cloudEnvName","os","os_version","browserName","browserVersion","url"})
     @BeforeMethod
-    public void setUp(@Optional("https://www.eBay.com/") String url)throws Exception{
+    public void setUp(@Optional("false") boolean useCloudEnv, @Optional("false") String cloudEnvName, @Optional("OS X") String os, @Optional("Sierra") String os_version, @Optional("chrome-options") String browsername,@Optional("69") String browserVersion,@Optional("https://www.eBay.com/") String url)throws Exception{
         System.setProperty("webdriver.chrome.driver","/Users/subhra/WebPageAutomation/Generic/Browser-Driver/chromedriver");
-        driver = new ChromeDriver();
+        if (useCloudEnv==true){
+            if (cloudEnvName.equalsIgnoreCase("browserstack")){
+                getCloudDriver(cloudEnvName,browserstack_username,browserstack_accesskey,os,os_version,browsername,browserVersion);
+            }else if (cloudEnvName.equalsIgnoreCase("saucelabs")){
+                getCloudDriver(cloudEnvName,saucelabs_username,saucelabs_accesskey,os,os_version,browsername,browserVersion);
+            }else{
+                getLocalDriver(os,browsername);
+            }
+        }
+
+        //driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(25, TimeUnit.SECONDS);
         driver.get(url);
+        driver.manage().window().maximize();
 
+    }
+
+    public WebDriver getLocalDriver(@Optional("mac") String OS, String browserName){
+        if (browserName.equalsIgnoreCase("chrome")){
+            if (OS.equalsIgnoreCase("OS X")){
+                System.setProperty("webdriver.chrome.driver","../Generic/Browser-Driver/chromedriver");
+            }else if (OS.equalsIgnoreCase("Windows")){
+                System.setProperty("webdriver.chrome.driver","../Generic/Browser-Driver/chromedriver.exe");
+            }
+            driver = new ChromeDriver();
+        }else if (browserName.equalsIgnoreCase("chrome-options")){
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disabled-notifications");
+            if (OS.equalsIgnoreCase("OS X")){
+                System.setProperty("webdriver.chrome.driver","../Generic/Browser-Driver/chromedriver");
+            }else if (OS.equalsIgnoreCase("Windows")){
+                System.setProperty("webdriver.chrome.driver","../Generic/Browser-Driver/chromedriver.exe");
+            }
+            driver = new ChromeDriver(options);
+        }
+
+        else if (browserName.equalsIgnoreCase("firefox")){
+            if (OS.equalsIgnoreCase("OS X")){
+                System.setProperty("webdriver.gecko.driver","../Generic/browser-driver/geckodriver");
+            }else if(OS.equalsIgnoreCase("Windows")){
+                System.setProperty("webdriver.gecko.driver","../Generic/browser-driver/geckodriver.exe");
+            }
+            driver = new FirefoxDriver();
+
+        }else if (browserName.equalsIgnoreCase("ie")){
+            System.setProperty("webdriver.ie.driver","../Generic/browser-driver/IEDriverServer.exe");
+        driver= new InternetExplorerDriver();
+        }
+return  driver;
+    }
+
+    public WebDriver getCloudDriver(String envName,String envUsername, String envAccessKey, String  os, String os_version, String browserName, String browserVersion) throws IOException {
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability("browser",browserName);
+        cap.setCapability("browser_version", browserVersion);
+        cap.setCapability("os",os);
+        cap.setCapability("os_version", os_version);
+        if (envName.equalsIgnoreCase("Saucelabs")){
+            driver = new RemoteWebDriver(new URL("http://"+envUsername+":"+ envAccessKey+"@ondemand.saucelabs.com:80/wd/hub"),cap);
+        }else if (envName.equalsIgnoreCase("Browserstack")){
+            cap.setCapability("resolution","1024x768");
+            driver = new RemoteWebDriver(new URL("http://" + envUsername + ":" + envAccessKey +
+                    "@hub-cloud.browserstack.com/wd/hub"), cap);
+        }
+        return driver;
     }
 
     @AfterMethod
     public void cleanUp(){
         driver.close();
     }
+
+
+
+
 }
