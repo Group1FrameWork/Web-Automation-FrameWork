20038a29d82d015889a5723e770d515bb82e2336
[mmursalin] <[mursalin3152@gmail.com]>
2018-09-21 00:19:52 -0400

    msnbc

---
+++ MSNBC/src/test/java/home/TestHomePage.java
@@ -0,0 +1,12 @@
+package home;
+
+import base.CommonAPI;
+import org.testng.annotations.Test;
+
+public class TestHomePage extends CommonAPI {
+
+    @Test
+    public void homeUrl(){
+        getCurrentPageUrl();
+    }
+}
