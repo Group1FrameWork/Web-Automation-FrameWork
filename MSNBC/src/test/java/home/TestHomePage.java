038c6662a78687d748dc923fdd658ec277dfdc05
[mmursalin] <[mursalin3152@gmail.com]>
2018-09-21 19:03:20 -0400

    19 clicks

--- MSNBC/src/test/java/home/TestHomePage.java
+++ MSNBC/src/test/java/home/TestHomePage.java
@@ -1,6 +1,7 @@
 package home;
 
 import base.CommonAPI;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class TestHomePage extends CommonAPI {
@@ -8,6 +9,14 @@ public class TestHomePage extends CommonAPI {
     @Test
     public void homeUrl(){
         getCurrentPageUrl();
-        navigateForward();
     }
+    @Test
+    public void clickWorld(){
+       driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/header/div[4]")).click();
+    }
+
+
+
+
+
 }
